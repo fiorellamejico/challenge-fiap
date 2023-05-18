8371158dbb25482c778336a1abddc7bbc9fd895b
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-05-18 16:27:30 -0300

    alterando extensão do cadastroSchema.

--- challenge-schneider/src/paginaInicial/index.jsx
+++ challenge-schneider/src/paginaInicial/index.jsx
@@ -1,5 +1,8 @@
 export default function PaginaInicial() {
     return (
-        <h1>Página inicial.</h1>
+        <main className=" text-gray-500">
+            <h1 className=" text-2xl text-sch-green">Bem-vindo à Rede Social Interna Schneider - Construindo um Futuro Sustentável! </h1>
+            <p className=""> Aqui, unimos funcionários em prol da sustentabilidade. Compartilhe ideias, colabore e inspire-se em práticas ecológicas no trabalho e na comunidade. Celebramos conquistas e reconhecemos esforços individuais. Todos têm um papel na construção de um futuro sustentável. Participe ativamente, compartilhe ideias e ajude a moldar nossa cultura sustentável. Juntos, transformamos esforços individuais em uma força coletiva. Construamos uma empresa responsável para o benefício de todos e do planeta. Bem-vindo à nossa Rede Social Schneider!</p>
+        </main>
     )
 }
