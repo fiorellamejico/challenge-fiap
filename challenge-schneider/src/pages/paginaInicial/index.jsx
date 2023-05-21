e2df707ff12b946ae0cc595c07d6fcb6db9421d0
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-05-21 15:13:58 -0300

    Alteracoes no responsivo

--- challenge-schneider/src/pages/paginaInicial/index.jsx
+++ challenge-schneider/src/pages/paginaInicial/index.jsx
@@ -11,7 +11,7 @@ export default function PaginaInicial() {
     const [isDialogOpen, setDialogOpen] = useState(true);
 
     return (
-        <main className=" overflow-hidden flex flex-col h-screen text-gray-500 bg-gradient-to-b from-gray-50 via-gray-100 to-gray-200">
+        <main className=" md:overflow-hidden flex flex-col h-screen text-gray-500 bg-gradient-to-b from-gray-50 via-gray-100 to-gray-200">
             <header className=' pt-2 flex justify-between absolute w-full h-16 '>
                 <img className=' ml-2 h-full ' src={logo} alt="logo schneider branco" />
                 <Link className=' flex justify-center items-center mr-2 shadow-xl transition-all bg-neutral-800 text-white text-2xl py-2 px-6 rounded hover:rounded-3xl font-bold cursor-pointer text-center ' to={'/cadastro'}><button className=' text-center'>Cadastre-se</button></Link>
@@ -21,7 +21,7 @@ export default function PaginaInicial() {
                 <img className=' w-full h-full object-cover ' src={PESSOAS_IMG} alt="Pessoas rindo imagem duotônica" />
             </div>
 
-            <div className=' md:overflow-hidden w-full flex flex-col md:flex-row h-1/2 '>
+            <div className=' w-full flex flex-col md:flex-row h-1/2 '>
                 <div className=' w-full md:w-1/2 p-6'>
                     <h1 className="  text-2xl lg:text-4xl text-sch-green">Bem-vindo à Rede Social Interna da Schneider.</h1>
                     <p className="text-justify text-xl font-thin">A SchneiderStream tem como objetivo unir funcionários em prol da sustentabilidade, compartilhando ideias e construindo um futuro sustentável coletivamente.</p>
