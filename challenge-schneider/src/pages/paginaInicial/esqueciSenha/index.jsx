66dd3edfce855d509c7bf093c6de623570a8a966
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-05-23 15:36:17 -0300

    ajustando responsivo "esqueci minha senha"

--- challenge-schneider/src/pages/paginaInicial/esqueciSenha/index.jsx
+++ challenge-schneider/src/pages/paginaInicial/esqueciSenha/index.jsx
@@ -35,7 +35,7 @@ export default function FormularioEsqueciSenha() {
     return (
 
         <form onSubmit={handleSubmit(inserirCliente)} className=" mt-2 pt-4 h-screen w-full flex justify-center items-center " >
-            <div className=" w-1/3 h-fit p-4 border rounded-xl">
+            <div className=" w-5/6 lg:w-1/3 h-fit p-4 border rounded-xl">
                 <h1 className=' text-sch-green text-2xl text-center'>Recuperação de Senha</h1>
                 <div className="mb-4">
                     <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
