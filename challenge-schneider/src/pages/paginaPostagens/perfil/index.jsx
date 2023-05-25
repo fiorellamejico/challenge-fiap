7e954242cb10c78ffc14fac8e04f041066c5a621
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-05-25 20:18:28 -0300

    Adicionando icone de edição de perfil.

--- challenge-schneider/src/pages/paginaPostagens/perfil/index.jsx
+++ challenge-schneider/src/pages/paginaPostagens/perfil/index.jsx
@@ -1,5 +1,5 @@
 import getUserData from '../schemas/usuarioTesteSchema'
-import { faHeart } from '@fortawesome/free-solid-svg-icons'
+import { faEdit, faHeart } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
@@ -15,6 +15,10 @@ export default function MeuPerfil() {
 
     const [windowOpen, setWindowOpen] = useState(true)
 
+    const action = () => {
+        alert("Função não implementada: Editar dados de usuário.")
+    }
+
     return (
         <div className=' h-screen flex flex-col  overflow-hidden justify-center items-center'>
             {windowOpen &&
@@ -25,13 +29,17 @@ export default function MeuPerfil() {
             }
             <div className=' mb-3 rounded-xl shadow-inner bg-neutral-200 '>
                 <div className=' w-full flex justify-center'>
+
                     <img className=' w-1/3 rounded-l-xl h-full obj-cover' src={user.fotoDePerfil} alt="foto de perfil" />
-                    <div className=' w-2/3 flex flex-col justify-center ml-4 text-neutral-800'>
-                        <p className=' text-xl font-bold '>Nome: {user.nome}</p>
-                        <p className=' text-lg'>Email: {user.email}</p>
-                        <p className=' text-lg'>Pontuação: <FontAwesomeIcon className=' text-sch-green' icon={faHeart} /> {user.pontos} </p>
-                        <p className=' text-lg  '> Ranking mensal: 24° lugar. </p>
-                        <p className=' text-neutral-600 text-sm italic'>&quot;{user.titulo}&quot;</p>
+                    <div className=' w-2/3 flex flex-col justify-start ml-4 text-neutral-800'>
+                        <FontAwesomeIcon onClick={() => action()} className=' hover:cursor-pointer m-4 self-end justify-self-start text-2xl' icon={faEdit} />
+                        <div>
+                            <p className=' text-xl font-bold '>Nome: {user.nome}</p>
+                            <p className=' text-lg'>Email: {user.email}</p>
+                            <p className=' text-lg'>Pontuação: <FontAwesomeIcon className=' text-sch-green' icon={faHeart} /> {user.pontos} </p>
+                            <p className=' text-lg  '> Ranking mensal: 24° lugar. </p>
+                            <p className=' text-neutral-600 text-sm italic'>&quot;{user.titulo}&quot;</p>
+                        </div>
                     </div>
                 </div>
             </div>
