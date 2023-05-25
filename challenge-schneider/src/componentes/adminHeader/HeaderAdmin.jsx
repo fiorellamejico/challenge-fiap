e4addb53e06d062ee2eac10d3d51da61a3c2647c
victobonetti <100859801+victobonetti@users.noreply.github.com>
2023-05-25 19:42:21 -0300

    encerrar sessão admin

--- challenge-schneider/src/componentes/adminHeader/HeaderAdmin.jsx
+++ challenge-schneider/src/componentes/adminHeader/HeaderAdmin.jsx
@@ -1,6 +1,17 @@
 import logo from '/images/schneider-logo-white.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faSignOut } from '@fortawesome/free-solid-svg-icons'
 
 export default function HeaderAdmin() {
+
+    const action = () => {
+        alert('Página em construção.')
+    }
+
+    const encerrarSessao = () => {
+        window.location.href = '/'
+    }
+
     return (
         <header className=" flex justify-between h-18 bg-neutral-900 text-white w-full">
             <div className=' m-2 flex items-center justify-center h-full'>
@@ -9,9 +20,10 @@ export default function HeaderAdmin() {
             </div>
             <nav className='  h-18 w-fit p-2 flex justify-center items-center'>
                 <ul className=' flex h-full justify-center items-center mr-4'>
-                    <li className=' cursor-pointer ml-4 hover:underline'>Usuários</li>
-                    <li className=' cursor-pointer ml-4 hover:underline'>Notícias</li>
-                    <li className=' cursor-pointer ml-4 hover:underline'>Postagens</li>
+                    <li onClick={() => action()} className=' cursor-pointer ml-4 hover:underline'>Usuários</li>
+                    <li onClick={() => action()} className=' cursor-pointer ml-4 hover:underline'>Notícias</li>
+                    <li onClick={() => action()} className=' cursor-pointer ml-4 hover:underline'>Postagens</li>
+                    <li onClick={() => encerrarSessao()} className=' ml-8 transition-all hover:bg-white hover:text-neutral-900 hover:shadow cursor-pointer border border-white p-1.5 rounded-xl flex justify-center items-center'> Encerrar sessão <FontAwesomeIcon className=' ml-2 text-2xl ' icon={faSignOut} /></li>
                 </ul>
             </nav>
         </header>
